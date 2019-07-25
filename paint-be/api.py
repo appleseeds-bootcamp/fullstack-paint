c2dd0da5bc353adb531e49101ca87b7fde7e0473
Matan Bakshi <matan.bakshi@gmail.com>
2019-07-25 22:32:15 +0300

    made this code work before intergration

---
+++ paint-be/api.py
@@ -0,0 +1,84 @@
+import bottle as b
+from bottle import get, post, put, delete
+import json
+import utils.db as db
+from utils.hashing import get_user_session_id, hash_password
+
+
+@get('/')
+def index():
+    username = b.request.get_cookie("username")
+    session_id = b.request.get_cookie("session_id")
+    res = b.static_file("/index.html", root="")
+    if not db.is_user_logged_in(username, session_id):
+        res.set_cookie('session_id', '', expires=0)
+        res.set_cookie('username', '', expires=0)
+    return res
+
+
+@post('/save')
+def save_painting():
+    username = b.request.get_cookie("username")
+    name = b.request.json.get("name")
+    painting = json.dumps(b.request.json.get("painting"))
+    try:
+        db.insert_new_painting(username, name, painting)
+        return {"saved": True}
+    except Exception as e:
+        print(e)
+        b.response.status = 500
+        b.response["status__line"] = "error saving painting in the DB"
+        return b.response
+
+
+@delete('/delete')
+def delete_painting():
+    username = b.request.get_cookie("username")
+    name = b.request.query.get("name")
+    try:
+        db.delete_painting(username, name)
+    except Exception as e:
+        print(e)
+        b.response.status = 500
+        b.response["status__line"] = "error writing to DB on save"
+    return b.response
+
+
+@get('/paintings')
+def get_paintings():
+    username = b.request.get_cookie("username")
+    try:
+        paintings = db.get_paintings(username)
+        return json.dumps({"paintings": paintings})
+    except Exception as e:
+        print(e)
+        return json.dumps("error writing to DB")
+
+
+@get('/painting')
+def get_painting():
+    username = b.request.get_cookie("username")
+    name = b.request.query.name
+    try:
+        painting = db.get_painting(username, name)
+        return json.dumps(painting)
+    except Exception as e:
+        print(e)
+        return json.dumps("error writing to DB")
+
+
+@post('/login')
+def signup():
+    username = b.request.forms.get("username")
+    password = b.request.forms.get("password")
+    hashed_password = hash_password(password)
+    session_id = b.request.get_cookie("session_id")
+    new_session_id = get_user_session_id()
+    if db.is_user_logged_in(username, session_id):
+        return b.static_file('index.html')
+    if not db.is_user_exist(username, hashed_password):
+        db.add_new_user(username, hashed_password, new_session_id)
+    db.update_user_session(username, hashed_password, new_session_id)
+    b.response.set_cookie("session_id", new_session_id)
+    b.response.set_cookie("username", username)
+    b.redirect('/')
---
+++ paint-be/utils/db.py
@@ -0,0 +1,97 @@
+import pymysql
+
+connection = pymysql.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    db="paint",
+    charset="utf8",
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+
+def insert_new_painting(username, name, painting):
+    with connection.cursor() as cursor:
+        query = "INSERT INTO paintings (username, name, painting) values ('{}', '{}', '{}' )".format(
+            username,
+            name,
+            painting
+        )
+        cursor.execute(query)
+        connection.commit()
+
+
+def delete_painting(username, name):
+    with connection.cursor() as cursor:
+        query = "DELETE FROM paintings WHERE username='{}' and name='{}' ".format(
+            username, name)
+        cursor.execute(query)
+        connection.commit()
+
+def get_paintings(username):
+    with connection.cursor() as cursor:
+        query = "SELECT name from paintings where username='{}'".format(
+            username)
+        cursor.execute(query)
+        paintings = cursor.fetchall()
+        return paintings
+
+def get_painting(username, name):
+    with connection.cursor() as cursor:
+        query = "SELECT painting from paintings where username='{}' and name='{}'".format(
+            username, name)
+        cursor.execute(query)
+        painting = cursor.fetchone()
+        return painting
+    
+def is_user_logged_in(username, session_id):
+    if not session_id:
+        return False
+    try:
+        with connection.cursor() as cursor:
+            query = "SELECT * FROM users WHERE username = '{}' AND sessionId = '{}'".format(
+                username, session_id)
+            cursor.execute(query)
+            result = cursor.fetchone()
+            return result is not None
+    except Exception as e:
+        print(e)
+        return False
+
+
+def is_user_exist(username, password):
+    try:
+        with connection.cursor() as cursor:
+            query = "SELECT * FROM users WHERE username = '{}' AND password = '{}'".format(
+                username, password)
+            cursor.execute(query)
+            result = cursor.fetchone()
+            return result is not None
+    except Exception:
+        return False
+
+
+def add_new_user(username, password, session_id):
+    try:
+        with connection.cursor() as cursor:
+            query = "INSERT INTO users (username, password, sessionId) values ('{}', '{}', '{}' )".format(
+                username,
+                password,
+                session_id
+            )
+            cursor.execute(query)
+            connection.commit()
+    except Exception as e:
+        print(e)
+        return json.dumps("error writing to DB")
+
+
+def update_user_session(username, password, session_id):
+    try:
+        with connection.cursor() as cursor:
+            query = "UPDATE users SET sessionId = '{}' WHERE username = '{}' AND password = '{}'".format(
+                session_id, username, password)
+            cursor.execute(query)
+            connection.commit()
+    except Exception as e:
+        print(e)
